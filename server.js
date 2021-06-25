8f44dbc7b936b5b53ef5bb1c5f985bd905708c18
CGSGAU3 <usikov_andru@mail.ru>
2021-06-25 18:17:12 +0300

    Debug

--- server.js
+++ server.js
@@ -217,36 +217,12 @@ mongoose.connect('mongodb+srv://olymppml30:AU3ID3MM5VB5@cluster0.cdj7z.mongodb.n
                     });
             });
             socket.on('changeState', (olympName, newState) => {
-
-                olympsCollection.findOne({ name: olympName },
-                    function (err, doc) {
-                        try {
-                            if (err) {
-                                throw err;
-                            }
-                            doc.state = newState;
-                            olympsCollection.save(doc);
-                        }
-                        catch (err) {
-                            console.log(err.message);
-                        }
-                    });
+                olympsCollection.updateOne({ name: olympName },
+                    { $set: { state: newState } });
             });
             socket.on('changeRegisterState', (olympName, newRegisterState) => {
-
-                olympsCollection.findOne({ name: olympName },
-                    function (err, doc) {
-                        try {
-                            if (err) {
-                                throw err;
-                            }
-                            doc.isRegisterOpen = newRegisterState;
-                            olympsCollection.save(doc);
-                        }
-                        catch (err) {
-                            console.log(err.message);
-                        }
-                    });
+                olympsCollection.updateOne({ name: olympName },
+                    { $set: { isRegisterOpen: newRegisterState } });
             });
 
 
